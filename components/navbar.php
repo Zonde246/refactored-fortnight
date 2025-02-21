3552279af3366ed267175806b900693bd7e92f7c
Zonde246 <72788587+Zonde246@users.noreply.github.com>
2025-02-21 14:41:52 +0530

    implement RWD into navbar

--- components/navbar.php
+++ components/navbar.php
@@ -9,12 +9,25 @@
         <div class="logo">
             <img src="/public/logo.png" width="106px" height="60px" />
         </div>
-        <div><button class="linkButtons">Home</button></div>
-        <div><button class="linkButtons">About us</button></div>
-        <div><button class="linkButtons">Contact us</button></div>
+        <div class="desktop"><button class="linkButtons">Home</button></div>
+        <div class="desktop"><button class="linkButtons">About us</button></div>
+        <div class="desktop"><button class="linkButtons">Contact us</button></div>
     </div>
     <div class="right">
-        <div id="login"><button class="linkButtons">Login</button></div>
-        <div id="user"><button class="linkButtons">Hello Zon!</button></div>
+        <div id="login" class="desktop"><button class="linkButtons">Login</button></div>
+        <div id="user" class="desktop"><button class="linkButtons">Hello Zon!</button></div>
+        <div class="mobile">
+            <button class="linkButtons" onclick="menuHandler()">Menu</button>
+        </div>
     </div>
+
 </nav>
+
+<div id="menu" class="closed">
+    <button class="linkButtons">Home</button>
+    <button class="linkButtons">About us</button>
+    <button class="linkButtons">Contact us</button>
+    <button class="linkButtons">Login</button>
+    <button class="linkButtons">Hello Zon!</button>
+    <button class="linkButtons" onclick="menuHandler()">Close</button>
+</div>
--- scripts/navbar/navbar.js
+++ scripts/navbar/navbar.js
@@ -3,3 +3,13 @@
 // Load local storage
 let session = localStorage.getItem('session');
 
+function menuHandler () {
+    let menu = document.querySelector('#menu');
+    if (menu.classList.contains('closed')) {
+        menu.classList.remove('closed');
+        menu.classList.add('menu--open');
+    } else {
+        menu.classList.remove('menu--open');
+        menu.classList.add('closed');
+    }
+}
